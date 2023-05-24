99ea9770ce5be99fc2366bc1c6c1892824b3ed03
nnh2x <ngonhathuy6878@gmail.com>
2023-05-24 22:06:16 +0700

    commit add to do list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomeLogin from "./src-login/Login";
 
 document.title = "Hooks";
 function App() {
   return (
-    <div>
+    <div style={{flex:1,justifyContent:'center',alignItems:'center'}}>
       <h1>Hello</h1>
+      <HomeLogin/>
     </div>
   );
 }
--- src/src-login/Login.tsx
+++ src/src-login/Login.tsx
@@ -1,10 +1,58 @@
-import React  from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable array-callback-return */
+import React, { useEffect, useState } from "react";
+import Home from "./Hello";
 
 const HomeLogin = () => {
+  const [textDefault, setTextDefault] = useState("Huy");
+  const [inputText, setInputText] = useState("");
+  const [pushArray, setPushArray] = useState<string[]>([]);
+
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextDefault(e.target.value);
+  };
+
+  const handleInputEnter = () => {
+    setInputText(textDefault);
+  };
+
+  const addToArray = () => {
+    setPushArray((prevArray) => [...prevArray, inputText]);
+  };
+
+  const deleteButton = (index: number) => {
+    setPushArray((prevData) => {
+      const pushArray = [...prevData];
+      pushArray.splice(index, 1);
+      return pushArray;
+    });
+  };
+
+  const RenderValue = () => {
+    return (
+      <>
+        {pushArray.map((array, index) => (
+          <div key={index}>
+            <h1>Test render: {array}</h1>
+            <button onClick={() => deleteButton(index)}>delete</button>
+          </div>
+        ))}
+      </>
+    );
+  };
+
   return (
-    <div>
-      <h1>Tests</h1>
+    <div style={{ flex: 1, justifyContent: "center" }}>
+      <h1>Login Form Name</h1>
+      <input type="text" onChange={handleInput} />
+      <Home names={inputText} />
+      <button onClick={handleInputEnter}>Click Add Name</button>
+      <button onClick={() => setInputText("")}>Reset Add Name</button>
+      <button onClick={addToArray}>Push Array</button>
+      <RenderValue />
     </div>
-  )
-}
+  );
+};
+
 export default HomeLogin;
