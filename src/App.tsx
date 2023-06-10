b724f67ba1a7128729c4a5b58e35cbe58e9160eb
nnh2x <ngonhathuy6878@gmail.com>
2023-06-11 01:55:55 +0700

    test HaskMap

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { useEffect, useState } from "react";
 import HomeAdd from "./Todo-list/HomeLogin/HomeAdd";
 import "./App.css"
 import Content from "./Todo-list/Mounted/Content";
+import DataConsole from "./Exdata/DataConsole";
 const App = () => {
   const [hide,setHide] = useState<Boolean>(false)
   const [title,setTitle] = useState<string>('')
   useEffect(() =>{
       setHide(false);
       document.title = title
-      console.log('title')
+      // console.log('title')
   },[title])
 
   return (
     <div>
          <div className="container" >
       <div>
+        <DataConsole/>
         <h1 id="add-users">Add User</h1>
         <HomeAdd/>
       </div>
---
+++ src/Exdata/DataConsole.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { Root } from "../Todo-list/Mounted/interFaceApi";
+
+const DataConsole = () => {
+  const [dataObject, setDataObject] = useState<Root>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        if (!response.ok) {
+          throw new Error("Error fetching data");
+        }
+        const data = await response.json();
+        setDataObject(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const dataName = ["Leanne Graham", "Clementina DuBuque", "Dennis Schulist "];
+  useEffect(() => {
+    dataObject.forEach((item, index) => {
+      const k = Object.keys(item);
+      const v = Object.values(item);
+      const haskMap = new Map<string, any>();
+      for (let i = 0; i < k.length; i++) {
+        haskMap.set(k[i], v[i]);
+      }
+
+      for (const [k, v] of haskMap) {
+        if (k === "name") {
+          console.log(k);
+        }
+      }
+      console.log(haskMap);
+    });
+  }, [dataName]);
+
+  return (
+    <div>
+      <h1>Ok</h1>
+    </div>
+  );
+};
+
+export default DataConsole;
